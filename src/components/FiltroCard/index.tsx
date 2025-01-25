6c70457788fa12c78bd6886badea3f3de2d6b198
Tronaluz <lucas_tron@hotmail.com>
2025-01-25 17:21:53 -0300

    feat: implement layout with BarraLateral and ListaDeTarefas components

---
+++ src/components/FiltroCard/index.tsx
@@ -0,0 +1,8 @@
+import * as S from './styles'
+
+const FiltroCard = () => (
+  <div>
+    <S.Contador>3</S.Contador>
+    <S.Label>pendentes</S.Label>
+  </div>
+)
---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,9 @@
+const BarraLateral = () => (
+  <aside>
+    <div>
+      <input type="text" placeholder="Buscar" />
+      <div></div>
+    </div>
+  </aside>
+)
+export default BarraLateral
