df4287c7539b30513a1f8eaab9281cf595929996
Tronaluz <lucas_tron@hotmail.com>
2025-02-04 12:34:04 -0300

    feat: add editing functionality to Tarefa component with conditional action buttons

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da Tarefa</S.Titulo>
-    <S.Tag>importante</S.Tag>
-    <S.Tag>Pendete</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Excluir</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+const Tarefa = () => {
+  const [estaEditando, setEstaEditando] = useState(false)
+  return (
+    <S.Card>
+      <S.Titulo>Nome da Tarefa</S.Titulo>
+      <S.Tag>importante</S.Tag>
+      <S.Tag>Pendete</S.Tag>
+      <S.Descricao />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao>Editar</S.Botao>
+            <S.Botao>Excluir</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
